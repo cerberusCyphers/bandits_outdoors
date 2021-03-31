78e66ef9553ab816c397a5ea61b6bed5f759f39a
Craig Colwell <ccolwell0683@gmail.com>
2021-03-31 17:01:43 -0400

    Ready to launch

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 'use strict';
 
-const email = document.getElementsByTagName('input');
-const btn = document.querySelector('button.btn');
+const email = document.getElementById('userInput');
+const btn = document.querySelector('.btn');
+const mobileNavClosed = document.querySelector('.ion-ios-paw');
+const mobileNavOpen = document.querySelector('.ion-ios-paw-outline');
+const mobileLinks = document.querySelector('.mobile-links');
 const mailingList = [];
 
-console.log(mailingList, email, btn);
+mobileNavClosed.addEventListener('click', function (e) {
+  mobileNavClosed.classList.add('hidden');
+  mobileNavOpen.classList.remove('hidden');
+  mobileLinks.style.display = 'block';
+});
+
+mobileNavOpen.addEventListener('click', function (e) {
+  mobileNavOpen.classList.add('hidden');
+  mobileNavClosed.classList.remove('hidden');
+  mobileLinks.style.display = 'none';
+});
 
 btn.addEventListener('click', function (e) {
-  e.preventDefault;
-  console.log('Click');
+  mailingList.push(email.value);
+  console.log('click', mailingList);
 });
